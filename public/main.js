6d7eebb9e1406ba2a0794871189d1a2f8375e4e4
toshiki <tosiki219@stu.kanazawa-u.ac.jp>
2023-06-25 19:00:23 +0900

    Function to register products and to retrieve product information from the DB. Bootstrap 5 applied.

--- public/main.js
+++ public/main.js
@@ -1,69 +1,72 @@
 window.onload = () => {
   loadProducts();
   loadCart();
-}
+};
 
 const purchaseProduct = (productId) => {
   fetch(`http://localhost:3000/products/${productId}/purchase`, {
-    method: 'POST'
+    method: "POST",
   })
-  .then(res => res.json())
-  .then(result => {
-    alert(result.message);
-    location.reload();
-  })
-  .catch(error => {
-    alert('Faild to purchase product');
-  });
-}
+    .then((res) => res.json())
+    .then((result) => {
+      alert(result.message);
+      location.reload();
+    })
+    .catch((error) => {
+      alert("Faild to purchase product");
+    });
+};
 
 const loadProducts = () => {
-  fetch('http://localhost:3000/products')
-    .then(response => response.json())
-    .then(products => {
-      let productDiv = document.getElementById('product-list');
-      products.forEach(product => {
-        let productElement = document.createElement('div');
+  fetch("http://localhost:3000/products")
+    .then((response) => response.json())
+    .then((products) => {
+      let productDiv = document.getElementById("product-list");
+      products.forEach((product) => {
+        let productElement = document.createElement("div");
         productElement.innerHTML = `
-          <h2>${product.name}</h2>
-          <p>${product.description}</p>
-          <p>Price: $${product.price}</p>
-          <p>Stock: ${product.stock}</p>
-          <button onclick="addToCart(${product.id})">Add to Cart</button>
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h2 class="card-title">${product.name}</h2>
+          <p class="card-text">${product.description}</p>
+          <p class="card-text">Price: $${product.price}</p>
+          <p class="card-text">Stock: ${product.stock}</p>
+          <button onclick="addToCart(${product.id})" class="btn btn-primary">Add to Cart</button>
+        </div>
+      </div>
+      
         `;
         productDiv.appendChild(productElement);
       });
     });
-}
+};
 
 const loadCart = () => {
-  fetch('http://localhost:3000/cart')
-    .then(res => res.json())
-    .then(cart => {
-      let cartDiv = document.getElementById('cart-list');
-      cart.forEach(product => {
+  fetch("http://localhost:3000/cart")
+    .then((res) => res.json())
+    .then((cart) => {
+      let cartDiv = document.getElementById("cart-list");
+      cart.forEach((product) => {
         productElement.innerHTML = `
         <h2>${product.name}</h2>
         <p>${product.description}</p>
         <p>Price: $${product.price}</p>
       `;
-      cartDiv.appendChild(productElement);
+        cartDiv.appendChild(productElement);
       });
     });
-}
-
-
+};
 
 const addToCart = (productId) => {
   fetch(`http://localhost:3000/products/${productId}/cart`, {
-    method: 'POST'
+    method: "POST",
   })
-  .then(res => res.json())
-  .then(result => {
-    alert(result.message);
-    location.reload();
-  })
-  .catch(error => {
-    alert('Failed to add product to cart');
-  });
-}
+    .then((res) => res.json())
+    .then((result) => {
+      alert(result.message);
+      location.reload();
+    })
+    .catch((error) => {
+      alert("Failed to add product to cart");
+    });
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const ProductModel = require("../models/Product.js");
 
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/submit", async (req, res) => {
+  const { id, name, description, price, stock } = req.body;
+  try {
+    await addProduct(id, name, description, price, stock);
+    res.send("Product saved!");
+  } catch (err) {
+    res.status(500).send("Error saving product!");
+  }
+});
 
 app.get("/products", async (req, res) => {
   const products = await ProductModel.find({});
@@ -20,6 +30,22 @@ app.get("/products/:id", async (req, res) => {
     res.json(product);
   }
 });
+
+const addProduct = async (id, name, description, price, stock) => {
+  const newProduct = new ProductModel({
+    id,
+    name,
+    description,
+    price,
+    stock,
+  });
+  try {
+    const result = await newProduct.save();
+    console.log("Product saved:", result);
+  } catch (err) {
+    console.error("Error saving product:", err);
+  }
+};
 
 /* app.post("/products/:id/purchase", (req, res) => {
   const productId = Number(req.params.id);
